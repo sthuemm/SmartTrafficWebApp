0acce2bf511937c29111dda3808806ec77e0fcbe
Sebastian Thümmel <sebastian.thuemmel@gmx.de>
2018-05-17 20:47:10 +0200

    frontend with buttons to trigger events, tables with values for traffic lights and input fields for traffic streams

--- src/main/java/de/htwg/smarttraffic/controller/HomeController.java
+++ src/main/java/de/htwg/smarttraffic/controller/HomeController.java
@@ -1,14 +1,30 @@
 package de.htwg.smarttraffic.controller;
 
+import de.htwg.smarttraffic.model.Intersection;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class HomeController {
 
 
     @GetMapping(path = "/")
-    public String getHomePage(){
+    public String getHomePage(Model model){
+        Intersection k1 = new Intersection(Intersection.IntersectionType.WNO);
+        Intersection k2 = new Intersection(Intersection.IntersectionType.WNOS);
+        Intersection k3 = new Intersection(Intersection.IntersectionType.WNO);
+
+        Map<String, Intersection> intersectionMap = new HashMap<>();
+        intersectionMap.put("K1", k1);
+        intersectionMap.put("K2", k2);
+        intersectionMap.put("K3", k3);
+
+        model.addAttribute("intersections", intersectionMap);
+
         return "index";
     }
 }
---
+++ src/main/java/de/htwg/smarttraffic/model/Intersection.java
@@ -0,0 +1,50 @@
+package de.htwg.smarttraffic.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Intersection {
+
+    public enum IntersectionType {
+        WNO, WSO, WNOS
+    }
+
+    private Map<String, Integer> trafficLights;
+
+    public Intersection(IntersectionType type){
+        trafficLights = new HashMap<>();
+        if ( type == IntersectionType.WNO){
+            setTrafficLightsForWNO();
+        } else if (type == IntersectionType.WSO){
+            setTrafficLightsForWSO();
+        } else if (type == IntersectionType.WNOS){
+            setTrafficLightsForWNOS();
+        }
+    }
+
+    private void setTrafficLightsForWNO(){
+        trafficLights.put("W <--> O", 2);
+        trafficLights.put("W <--> N", 2);
+        trafficLights.put("O <--> N", 2);
+    }
+
+    private void setTrafficLightsForWSO(){
+        trafficLights.put("W <--> O", 2);
+        trafficLights.put("W <--> S", 2);
+        trafficLights.put("O <--> S", 2);
+    }
+
+    private void setTrafficLightsForWNOS(){
+        trafficLights.put("W <--> O", 2);
+        trafficLights.put("W <--> N / O <--> S", 2);
+        trafficLights.put("S <--> N", 2);
+    }
+
+    public Map<String, Integer> getTrafficLights() {
+        return trafficLights;
+    }
+
+    public void setTrafficLights(Map<String, Integer> trafficLights) {
+        this.trafficLights = trafficLights;
+    }
+}
